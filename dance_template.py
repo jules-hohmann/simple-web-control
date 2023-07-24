23a8a074c431eae6721d40f42cda667660d7b621
Jules <jules.hohmann@gmail.com>
2023-07-24 08:53:48 -0400

    added movement functions and connection

--- dance_template.py
+++ dance_template.py
@@ -1,8 +1,24 @@
 from motor_test import test_motor
 import time
 from pymavlink import mavutil
-
-
+import socket
+ 
+'''sets up socket Host'''
+HOST = "0.0.0.0"
+'''sets up the port'''
+Port = 8000
+socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+'''with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, Port))
+    s.listen(1)
+    conn, addr = s.accept()
+    with conn:
+        print ('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data: break
+            conn.sendall(data)
+            '''
 def arm_rov(mav_connection):
     """
     Arm the ROV, wait for confirmation
@@ -33,7 +49,7 @@ def run_motors_timed(mav_connection, seconds: int, motor_settings: list) -> None
     while step < seconds:
         for i in range(len(motor_settings)):
             test_motor(mav_connection=mav_connection, motor_id=i, power=motor_settings[i])
-        time.sleep(0.2)
+        # time.sleep(0.2)
         step += 0.2
 
     
@@ -47,13 +63,6 @@ if __name__ == "__main__":
     # Arm the ROV and wait for confirmation
     arm_rov(mav_connection)
 
-    ####
-    # Run choreography
-    ####
-    """
-    Call sequence of calls to run_timed_motors to execute choreography
-    Motors power ranges from -100 to 100
-    """
     def forwardlinesegment(t, m):
          run_motors_timed(mav_connection, seconds=t, motor_settings=[m, m, -m, -m, 0, 0])
          run_motors_timed(mav_connection, seconds=t/2, motor_settings=[-m/4, -m/4, m/4, m/4, 0, 0])
@@ -61,9 +70,55 @@ if __name__ == "__main__":
          run_motors_timed(mav_connection, seconds=t, motor_settings=[-m, m, m, -m, 0, 0])
     def rightturn(t, m):
          run_motors_timed(mav_connection, seconds=t, motor_settings=[m, -m, -m, m, 0, 0])
+    def rightstrafe(t, m):
+         run_motors_timed(mav_connection, seconds = t, motor_settings= [m, -m, m, -m])
+    def leftstrafe(t, m):
+         run_motors_timed(mav_connection, seconds=t, motor_settings= [ -m, m, -m, m])
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, Port))
+        s.listen(1)
+        print("got here")
+        conn, addr = s.accept()
+        print(HOST)
+        with conn:
+            print("Address: ", addr)
+            while True:
+
+                data = conn.recv(1024)
+                data = data.decode('UTF-8')
+                if not data: break
+               
+                if data.strip() == "forward":
+                    print("data received")
+                    run_motors_timed(mav_connection, seconds=10, motor_settings=[-100, -100, 100, 100, 0, 0])
+                    arm_rov(mav_connection)
+                if data.strip() == "right turn":
+                    print("data received")
+                    rightturn(5, 100)
+                    arm_rov(mav_connection)
+                if data.strip() == "left turn":
+                    print("data received")
+                    leftturn(5, 100)
+                    arm_rov(mav_connection)
+                if data.strip() == "left":
+                    print("data received")
+                    leftstrafe(10, 100)
+                    arm_rov(mav_connection)
+                if data.strip() == "right":
+                    print("data received")
+                    rightstrafe(10, 100)
+                    arm_rov(mav_connection)
+                else:
+                    continue
+    ####
+    # Run choreography
+    ####
+    """
+    Call sequence of calls to run_timed_motors to execute choreography
+    Motors power ranges from -100 to 100
+    """
+   
     def draw_Mit(S):
-        #M
-        
         forwardlinesegment(4*S, 100)
         rightturn(3, 100)
         forwardlinesegment(4*S, 100)
@@ -82,24 +137,10 @@ if __name__ == "__main__":
         forwardlinesegment(2.5*S, -100)
         rightturn (2, 75)
         forwardlinesegment(4*S, 100)
-        #I
-        '''
-        run_motors_timed(mav_connection, seconds=5, motor_settings=[50, 0, 0, -50, 0, 0])
-        run_motors_timed(mav_connection, seconds=2, motor_settings=[50, 50, -50, -50, 0, 0])
-        run_motors_timed(mav_connection, seconds=2, motor_settings=[-25, -25, 25, 25, 0, 0])
-        run_motors_timed(mav_connection, seconds=5, motor_settings=[100, 0, 0, -100, 0, 0])
-        run_motors_timed(mav_connection, seconds=2, motor_settings=[100, 100, -100, -100, 0, 0])
-        run_motors_timed(mav_connection, seconds=5, motor_settings=[0, 100, -100, 0, 0, 0])
-        run_motors_timed(mav_connection, seconds=2, motor_settings=[-25, 25, -25, -25, 0, 0])
-        #last part of I connecting to T
-        run_motors_timed(mav_connection, seconds=4, motor_settings=[100, 100, -100, -100, 0, 0])
-        run_motors_timed(mav_connection, seconds=2, motor_settings=[-25, 25, -25, -25, 0, 0])
-        run_motors_timed(mav_connection, seconds=5, motor_settings=[0, 100, -100, 0, 0, 0])
-        run_motors_timed(mav_connection, seconds=2, motor_settings=[100, 100, -100, -100, 0, 0])
-    '''
+       
     
 
-    draw_Mit(2)
+    
 
     ####
     # Disarm ROV and exit
